cc0870ae062fcb15c26d48af8ee0a452aca1fbfa
Julio Branda <kolstomier@protonmail.com>
2023-03-28 17:09:02 -0300

    ejPractico3 guia 3

--- src/guia3/ejerciciosPracticos/ejPractico3.java
+++ src/guia3/ejerciciosPracticos/ejPractico3.java
@@ -2,15 +2,16 @@ package guia3.ejerciciosPracticos;
 
 import java.util.Scanner;
 
-public class ejPractico2 {
+public class ejPractico3 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese una frase");
         String frase = leer.nextLine();
-        if (frase.equals("eureka")) {
-            System.out.println("Correcto");
+
+        if (frase.length() == 8) {
+            System.out.println("CORRECTO");
         } else {
-            System.out.println("Incorrecto");
+            System.out.println("INCORRECTO");
         }
     }
 }
