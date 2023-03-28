82a9f3dd751de3bc6a0e498b524b4c4ef0b67341
Julio Branda <kolstomier@protonmail.com>
2023-03-28 16:44:15 -0300

    ejPractico1 guia 3

---
+++ src/guia3/ejercicioPracticos/ejPractico1.java
@@ -0,0 +1,16 @@
+package guia3.ejercicioPracticos;
+
+import java.util.Scanner;
+
+public class ejPractico1 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        int num = leer.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("El número es par");
+        } else {
+            System.out.println("El número es impar");
+        }
+    }
+}
