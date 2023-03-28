c21c70cfb2b252c59ab701a4abf9e51b8ca15217
Julio Branda <kolstomier@protonmail.com>
2023-03-28 13:32:39 -0300

    ejercicio8 guia 3

---
+++ src/guia3/ejercicio8.java
@@ -0,0 +1,18 @@
+package guia3;
+
+import java.util.Scanner;
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+
+        int nota = -1;
+
+        while(nota < 0 || nota > 10) {
+            Scanner leer = new Scanner(System.in);
+            System.out.println("Ingrese una nota entre 0 y 10");
+            nota = leer.nextInt();
+        }
+        System.out.println("La nota ingresada es " + nota);
+    }
+
+}
