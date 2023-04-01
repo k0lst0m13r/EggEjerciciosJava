4821c69c19d8301a4df90d35fef738bc38e4adb7
Julio Branda <kolstomier@protonmail.com>
2023-04-01 19:58:33 -0300

    ejPractico4 guia 4

---
+++ src/guia4/ejerciciosPracticos/ejPractico4.java
@@ -0,0 +1,21 @@
+package guia4.ejerciciosPracticos;
+
+import java.util.Scanner;
+
+public class ejPractico4 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Ingrese un número para saber si es primo");
+        int numero = num.nextInt();
+
+        System.out.println(esPrimo(numero));
+    }
+
+    public static boolean esPrimo(int numero) {
+        if ((numero % 2 == 0 || numero % 3 == 0) && (numero != 2 && numero != 3)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
