17920c41ae9fc2ca718f3e63b0a9ba86666f0231
Julio Branda <kolstomier@protonmail.com>
2023-04-12 21:06:35 -0300

    ejercicio  guia 6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import guia6.Entidad.Persona;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ src/guia6/Entidad/Persona.java
@@ -0,0 +1,38 @@
+package guia6.Entidad;
+
+public class Persona {
+    String nombre;
+    int edad;
+    String ocupacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+
+    public Persona(String nombre, int edad, String ocupacion){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ocupacion = ocupacion;
+    }
+
+}
